295541660dc7a068c330aa983b60703dcf969d8a
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-12 13:46:23 -0500

    Added inject manifest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
-const { GenerateSW, InjectManifest } = require('workbox-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -24,7 +24,11 @@ module.exports = () => {
         title: 'Text Editor',
       }),
       new MiniCssExtractPlugin(),
-      new GenerateSW()
+      new WorkboxPlugin.GenerateSW(),
+      new WorkboxPlugin.InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'service-worker.js',
+      })
     ],
 
     module: {
