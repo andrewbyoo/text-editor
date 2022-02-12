de39d6693c6f8dffbc0e900bab9a7df5de0e7a77
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-12 15:34:19 -0500

    Changing how buttons are handled

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,18 +2,12 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 window.addEventListener('beforeinstallprompt', (event) => {
-  event.preventDefault();
-  butInstall.style.visibility = 'visible';
-
-  butInstall.addEventListener('click', () => {
-    event.prompt();
-    butInstall.setAttribute('disabled', true);
-    butInstall.textContent = 'Installed!';
-  });
+  window.deferredPrompt = event;
+  butInstall.classList.toggle('hidden', false);
 });
 
-window.addEventListener('appinstalled', (event) => {
-  event.preventDefault();
-  butInstall.setAttribute('disabled', true);
-  butInstall.style.display = 'none';
-});
+// TODO: Implement a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {});
+
+// TODO: Add an handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {});
