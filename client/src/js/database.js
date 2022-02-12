45d6767f766800ba1706d9401ca51db501a04f34
Andrew Yoo <andrewbyoo@gmail.com>
2022-02-12 15:02:59 -0500

    Added put logic

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,8 +12,14 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (id, content) => {
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: id, jate: content });
+  const result = await request;
+  console.log(`Database has been update with: ${result}`);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
